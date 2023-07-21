1bef4c5a6631672b35701890cd7f01fbe0ba74f7
MelgarejoM <martamelgarejo@hotmail.com>
2023-07-21 19:07:18 -0300

    React-Hooks

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,11 +1,74 @@
-import React from "react";
-import {connect} from 'react-redux'
+import React, { useState } from "react";
+import {connect, useDispatch} from 'react-redux'
 import Card from '../Card/Card'
+import { filterCards, orderCards } from "../../redux/actions";
+
 
 function Favorites({myFavorites}){
+    const [orderBy, setOrderBy] = useState("A"); // Estado local para guardar el orden seleccionado
+    const [filterByGender, setFilterByGender] = useState("All");
+    const [aux, setAux] = useState(false); // Agregar el estado local aux e inicializarlo en false
+    const dispatch = useDispatch(); // Agregar el hook useDispatch
+
+    
+    const handleChangeOrder = (event) => {
+        setOrderBy(event.target.value); // Actualizar el estado local con el nuevo valor seleccionado
+        handleOrder(event); // Llamar a la función handleOrder al cambiar el orden
+    }
+
+    const handleChangeGenderFilter = (event) => {
+        setFilterByGender(event.target.value);
+        handleFilter(event); // Llamar a la función handleFilter al cambiar el filtro de género
+    
+    };
+
+    // Función para despachar la acción orderCards
+  const handleOrder = (event) => {
+    setAux(!aux); // Cambiar el valor de aux al despachar la acción
+    dispatch(orderCards(event.target.value)); // Despachar la acción para ordenar las tarjetas
+  };
+
+   // Función para despachar la acción filterCards
+   const handleFilter = (event) => {
+    setAux(!aux); // Cambiar el valor de aux al despachar la acción
+    dispatch(filterCards(event.target.value)); // Despachar la acción para filtrar las tarjetas
+  };
+
+    // Aplicar el orden seleccionado al array de personajes favoritos
+    const orderedFavorites = myFavorites.slice().sort((a, b) => {
+        if (orderBy === "A") {
+            return a.id - b.id; // Orden ascendente
+        } else if (orderBy === "D") {
+            return b.id - a.id; // Orden descendente
+        }
+        return 0;
+    });
+
+
+
+    const filteredFavorites =
+    filterByGender === "All"
+      ? orderedFavorites
+      : orderedFavorites.filter((character) => character.gender === filterByGender);
+
     return (
-        <div>{
-            myFavorites?.map(({id, name, gender, species, image,status, origin}) => (<Card
+        <div>
+           
+            <select value={orderBy} onChange={handleChangeOrder}>
+                <option value="A">Ascendente</option>
+                <option value="D">Descendente</option>
+            </select>
+
+        
+            <select value={filterByGender} onChange={handleChangeGenderFilter}>
+                <option value="All">Todos</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Genderless">Genderless</option>
+                <option value="unknown">Unknown</option>
+            </select>
+            
+            {filteredFavorites.map(({id, name, gender, species, image,status, origin}) => (<Card
                 key= {id}
                 id={id}
                 name={name}
@@ -13,10 +76,9 @@ function Favorites({myFavorites}){
                 species={species}
                 image={image}
                 status={status}
-                origin={origin} />))
-            
-        }</div>
-    )
+                origin={origin} />))}
+        </div>
+    );
 }
 
 export function mapStateToProps(state){
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,15 +1,27 @@
-import { ADD_FAV, REMOVE_FAV } from "./action-type";
+import { ADD_FAV, REMOVE_FAV, } from "./action-type";
 
 const  initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
+}
+
+function compareAsc(a, b) {
+    return a.id - b.id;
+}
+
+function compareDesc(a, b) {
+    return b.id - a.id;
 }
 
 function rootReducer (state= initialState, action){
     switch(action.type){
         case ADD_FAV:
+            const newCharacter = action.payload;
+
             return {
                 ...state,
-                myFavorites: [...state.myFavorites, action.payload]
+                myFavorites: [...state.myFavorites, newCharacter],
+                allCharacters: [...state.allCharacters, newCharacter ]
             }
 
             case REMOVE_FAV:
@@ -17,8 +29,35 @@ function rootReducer (state= initialState, action){
                 ...state,
                 myFavorites: state.myFavorites.filter(char => char.id !== action.payload) 
             }
+
+            case "FILTER":
+                const genreToFilter = action.payload;
+                const filteredCharacters = state.allCharacters.filter(char => char.gender === genreToFilter);
+
+                return {
+                    ...state,
+                    myFavorites: filteredCharacters 
+            }
+
+
+            case "ORDER":
+                 const orderType = action.payload;
+                 const sortedCharacters = [...state.myFavorites];
+
+                  if (orderType === "A") {
+                  sortedCharacters.sort(compareAsc);
+                  } else if (orderType === "D") {
+                   sortedCharacters.sort(compareDesc);
+                 }
+
+                return {
+                    ...state,
+                    myFavorites: sortedCharacters
+            }
+
             default:
                 return state
+
     }
 
 }
